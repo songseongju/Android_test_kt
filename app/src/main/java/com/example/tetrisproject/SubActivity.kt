a33c5412c98ffcacfc6f94c129a6945d4e2f1fe4
[songseongju] <[sj88139@naver.com]>
2023-11-27 21:23:06 +0900

    안드로이드 뷰 바인딩

---
+++ app/src/main/java/com/example/tetrisproject/SubActivity.kt
@@ -0,0 +1,26 @@
+package com.example.tetrisproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tetrisproject.databinding.ActivityMainBinding
+
+class SubActivity : AppCompatActivity() {
+
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: ActivityMainBinding? = null
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    // 액티비티가 파괴될 때..
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroy()
+    }
+}
